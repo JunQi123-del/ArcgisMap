a99df080566d6aa028938b63de950dd4db083d22
Jun Qi <tanjunqi47@gmail.com>
2026-08-06 12:51:36 +0800

    created the layout for webpage

--- my-arcgis-app/src/App.tsx
+++ my-arcgis-app/src/App.tsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 //   appId: "YOUR_APP_ID",
 // });
 
+
 // Individual imports for each Map, Chart and Calcite component
 import "@arcgis/map-components/components/arcgis-expand";
 import "@arcgis/map-components/components/arcgis-legend";
@@ -21,90 +22,50 @@ import "@esri/calcite-components/components/calcite-navigation";
 import "@esri/calcite-components/components/calcite-navigation-logo";
 
 // Import modules and types from the SDK's core API
-import Graphic from "@arcgis/core/Graphic.js";
-import Point from "@arcgis/core/geometry/Point.js";
-import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol.js";
-import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol.js";
-import type WebMap from "@arcgis/core/WebMap.js";
+import Map from "@arcgis/core/Map.js";
+import MapView from "@arcgis/core/views/MapView.js";
+import {useEffect,useRef} from "react";
+import mapView from "@arcgis/core/views/MapView";
 
-export function App(): React.JSX.Element {
-  const [navHeading, setNavHeading] = useState("");
-  const [navDescription, setNavDescription] = useState("");
+export default function App() {
 
-  const handleViewReady = (event: CustomEvent): void => {
-    const viewElement = event.target as HTMLArcgisMapElement;
+  const mapDivRef = useRef<HTMLDivElement>(null);
 
-    // Use metadata from the Web Map to populate the header
-    const map = viewElement.map as WebMap;
-    const portalItem = map.portalItem;
-    const title = portalItem?.title ? portalItem.title : "A web map";
-    const description = portalItem?.description ? portalItem.description : "ArcGIS Maps SDK for JavaScript template";
+  useEffect(() =>{
+     if (!mapDivRef.current) return;
 
-    setNavHeading(title);
-    setNavDescription(description);
+     const map = new Map({
+      basemap: "hybrid"
+     });
 
-    // Define a point geometry
-    const point = new Point({
-      longitude: -98.38,
-      latitude: 38.34,
-    });
+     const view = new MapView({
+      container: mapDivRef.current,
+      map: map,
+      center: [103.8198, 1.3521],
+      zoom: 11,
+     });
 
-    // Create an outline for the marker symbol
-    const outline = new SimpleLineSymbol({
-      color: "white",
-      width: 2,
-    });
+     return () => view.destroy();
 
-    // Create a symbol for drawing the point
-    const symbol = new SimpleMarkerSymbol({
-      style: "triangle",
-      size: 20,
-      color: "red",
-      outline,
-    });
-
-    // Create a graphic and add the geometry and symbol to it
-    const pointGraphic = new Graphic({
-      geometry: point,
-      symbol,
-    });
-
-    // Add a graphic to demonstrate custom visualizations beyond Web Map content
-    viewElement.graphics.add(pointGraphic);
-  };
+  },[]);
 
   return (
-    // The Shell component is used as a layout for this template
-    <calcite-shell content-behind>
-      <calcite-navigation slot="header">
-        {/* Heading and description dynamically populated */}
-        <calcite-navigation-logo
-          heading={navHeading}
-          description={navDescription}
-          heading-level="1"
-          slot="logo"
-        ></calcite-navigation-logo>
-      </calcite-navigation>
-      {/* The Map component fits to the size of the parent element  */}
-      {/* The basemap, extent, zoom and more are provided by the Web Map (item-id) */}
-      {/* Note: popup-component-enabled enables the Popup component (beta). See https://developers.arcgis.com/javascript/latest/references/map-components/components/arcgis-map/#popupComponentEnabled for details. */}
-      <arcgis-map
-        item-id="dd4b2f25487d4a37a45093ba6acd026d"
-        popup-component-enabled
-        onarcgisViewReadyChange={handleViewReady}
-      >
-        <arcgis-zoom slot="top-left" />
-        <arcgis-search slot="top-right" />
-        <arcgis-expand slot="bottom-left">
-          <arcgis-legend></arcgis-legend>
-        </arcgis-expand>
-        {/*  A Feature Layer in the Web Map has an associated chart (layer-item-id) */}
-        <arcgis-chart
-          layer-item-id="b1717962dab247ad93eaca257b32fe02"
-          chart-index="1"
-          slot="bottom-right"
-        ></arcgis-chart>
-      </arcgis-map>
-    </calcite-shell>
+    <div className="app-shell">
+      <header className="app-toolbar">
+        <span>GeoOps</span>
+      </header>
+
+      <div className="app-body">
+        <aside className = "app-sidebar">sidebar</aside>
+        <main className = "app-map" ref={mapDivRef}></main>
+        <aside className="app-detail">detail panel</aside>
+      </div>
+  <footer className= "app-statusbar">status bar</footer>
+  </div>
+
+
+  
   );
+
 }
+
