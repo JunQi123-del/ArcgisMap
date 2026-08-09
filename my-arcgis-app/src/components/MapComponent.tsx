2d88154857f8fb2e7e538aecfe953bb17e7ef91c
Jun Qi <tanjunqi47@gmail.com>
2026-08-09 13:00:54 +0800

    commit added changes

---
+++ my-arcgis-app/src/components/MapComponent.tsx
@@ -0,0 +1,14 @@
+import baseMapConfig from "@/config/mapConfig.json"
+import "@arcgis/map-components/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-zoom";
+
+export default function MapCanvas(){
+    return (
+        <arcgis-map
+        basemap={baseMapConfig.baseMap}
+        center={baseMapConfig.center}
+        zoom={baseMapConfig.zoom}
+        style={{ position: "absolute", inset: 0 }}
+        />
+    )
+}
